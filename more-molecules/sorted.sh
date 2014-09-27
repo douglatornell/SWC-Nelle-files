4f3d7cd0e171e57cb490b22478204694285a0ed3
Doug Latornell <djl@douglatornell.ca>
2014-09-27 17:51:09 +0000

    Add documentation.

--- more-molecules/sorted.sh
+++ more-molecules/sorted.sh
@@ -1,1 +1,5 @@
+# Print the results of wc -l (number of lines and filenames)
+# sorted in order of increasing line count
+# for the filename patterns given on the command-line.
+
 wc -l $* | sort -n
